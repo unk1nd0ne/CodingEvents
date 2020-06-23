f54adc747f54efdfe0dd096e635f16ecdc6bade0
unk1nd0ne <z.neil.schaefer@gmail.com>
2020-06-23 10:19:31 -0500

    Add project files.

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    [Route("Events")]
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+        //GET: events
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.events = Events;
+
+            return View();
+        }
+
+        [HttpGet("Add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("Add")]
+        public IActionResult AddEvent(string name)
+        {
+            Events.Add(name);
+
+            return Redirect("/Events");
+        }
+    }
+}
